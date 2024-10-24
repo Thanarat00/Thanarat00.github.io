664c9854db7c394f14fec8a86c47a025aa52dfdf
tan <bahoza341@example.com>
2024-10-24 23:43:26 +0700

    sdf

--- src/App.tsx
+++ src/App.tsx
@@ -11,3 +11,30 @@ const App = () => {
 
 export default App;
 
+
+// import React, { useEffect, useState } from 'react';
+// import flagsData from './assets/flags.json';
+
+// const App = () => {
+//     const [flags, setFlags] = useState([]);
+
+//     useEffect(() => {
+//         setFlags(flagsData);
+//     }, []);
+
+//     return (
+//         <div>
+//             <h1>Flags of Countries</h1>
+//             <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+//                 {flags.map((flag) => (
+//                     <div key={flag.src}>
+//                         <img src={flag.src} alt={flag.title} style={{ width: '300px', margin: '10px' }} />
+//                         <p>{flag.title}</p>
+//                     </div>
+//                 ))}
+//             </div>
+//         </div>
+//     );
+// };
+
+// export default App;
--- src/components/Read_excel.tsx
+++ src/components/Read_excel.tsx
@@ -1,61 +1,21 @@
-import { useState, useEffect } from "react";
-import { Bar } from "react-chartjs-2";
-import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend,
-} from "chart.js";
+
+import { useState, useEffect, useRef } from "react";
 import { ContinentVisibility, CountryData } from "../Type/excel.type";
 import { FaPlay } from "react-icons/fa";
 import { TbPlayerPauseFilled } from "react-icons/tb";
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
+
 import Papa from "papaparse";
 import countriesList from "../countries/countries";
-
-ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  BarElement,
-  Title,
-  Tooltip,
-  Legend
-);
-
-const drawBarValues = {
-  id: "drawBarValues",
-  afterDatasetsDraw(chart: any) {
-    const { ctx } = chart;
-    chart.data.datasets.forEach((dataset: any, i: number) => {
-      const meta = chart.getDatasetMeta(i);
-      meta.data.forEach((bar: any, index: number) => {
-        const value = dataset.data[index];
-        if (value !== null) {
-          const xPos = bar.x + 10;
-          const yPos = bar.y + 5;
-          ctx.fillStyle = "rgba(0, 0, 0, 0.6)";
-          ctx.font = "bold 12px Arial";
-          ctx.fillText(
-            value.toLocaleString(undefined, { maximumFractionDigits: 0 }),
-            xPos,
-            yPos - 5
-          );
-        }
-      });
-    });
-  },
-};
-
-ChartJS.register(drawBarValues);
+import flagsData from "../assets/flags.json";
 
 const Read_excel = () => {
   const [excelData, setExcelData] = useState<CountryData[]>([]);
   const [selectedYear, setSelectedYear] = useState<number>(1950);
   const [populationData, setPopulationData] = useState<number[]>([]);
   const [isAnimating, setIsAnimating] = useState<boolean>(false);
-  const [selectedYears, setSelectedYears] = useState<number[]>([1950]);
+  const chartRef = useRef(null);
   const [visibleContinents, setVisibleContinents] = useState<
     ContinentVisibility | any
   >({
@@ -92,12 +52,15 @@ const Read_excel = () => {
               const year = i["Year"];
               const population = i["Population"];
               const totalpopulation = Number(population);
+              const flag = flagsData.find((flag) => flag.title === CountryName);
+              const image = flag ? flag.src : "";
 
               return {
                 country: CountryName,
                 year: Number(year),
                 population: totalpopulation,
-                continent: continentMap[CountryName],
+                // continent: continentMap[CountryName],
+                image: image,
               };
             });
             setExcelData(country);
@@ -110,6 +73,27 @@ const Read_excel = () => {
 
     fetchExcelFile();
   }, []);
+
+  
+  useEffect(() => {
+    if (!isAnimating || selectedYear >= 2021) {
+      if (selectedYear === 2021) {
+        setIsAnimating(false);
+      }
+      return;
+    }
+  
+    const nextYear = selectedYear + 1;
+    const newPopulationData = getPopulationData(nextYear);
+  
+    const interval = setInterval(() => {
+      animatePopulationData(populationData, newPopulationData);
+      setSelectedYear(nextYear);
+    }, 500);
+
+    return () => clearInterval(interval);
+  }, [isAnimating, selectedYear, populationData]); 
+  
 
   const continentColors: Record<string, string> = {
     Asia: "#664df6",
@@ -131,66 +115,20 @@ const Read_excel = () => {
       });
   };
 
-  const sortedPopulationData =
-    populationData.length > 0
-      ? populationData
-      : getPopulationData(selectedYear);
-  const countriesWithPopulation = countries
-    .filter((country) => visibleContinents[continentMap[country]])
-    .map((country, index) => ({
-      country,
-      population: sortedPopulationData[index],
-    }))
-    .sort((a, b) => b.population - a.population)
-    .slice(0, 12);
-  const sortedCountries = countriesWithPopulation.map((item) => item.country);
-  const sortedPopulations = countriesWithPopulation.map(
-    (item) => item.population
-  );
-  const totalPopulation = sortedPopulations.reduce(
-    (total, num) => total + num,
-    0
-  );
-
-  const animatePopulationData = (startData: number[], endData: number[]) => {
-    const steps = 100;
-    const increment = endData.map(
-      (end, index) => (end - startData[index]) / steps
-    );
-    let currentStep = 0;
-    const animate = () => {
-      if (currentStep >= steps) {
-        setPopulationData(endData);
-        return;
-      }
-      setPopulationData(
-        startData.map((start, index) =>
-          Math.round(start + increment[index] * currentStep)
-        )
-      );
-      currentStep++;
-      requestAnimationFrame(animate);
-    };
-
-    animate();
-  };
-
-  useEffect(() => {
-    if (!isAnimating) return;
-    if (selectedYear === 2021) {
-      setIsAnimating(false);
-      return;
-    }
-    const interval = setInterval(() => {
-      const nextYear = selectedYear + 1;
-      const newPopulationData = getPopulationData(nextYear);
-      animatePopulationData(populationData, newPopulationData);
-      setSelectedYear(nextYear);
-      setSelectedYears((prvData) => [...prvData, selectedYear + 1]);
-    }, 100);
-
-    return () => clearInterval(interval);
-  }, [selectedYear, populationData, isAnimating]);
+  const sortedPopulationData = populationData.length > 0? populationData: getPopulationData(selectedYear);
+  const countriesWithPopulation = countries.filter((country) => visibleContinents[continentMap[country]]).map((country, index) => ({country, population: sortedPopulationData[index], })).sort((a, b) => b.population - a.population).slice(0, 12);
+  const sortedPopulations = countriesWithPopulation.map((item) => item.population );
+  const totalPopulation = sortedPopulations.reduce((total, num) => total + num, 0);
+  const outputArray = Object.values(countriesWithPopulation).map((item) => {
+    const continent = continentMap[item.country];
+    const flag = flagsData.find((flag) => flag.title === item.country);
+    const image = flag ? flag.src : ""; 
+    return [
+      item.country,
+      item.population,
+      { color: continentColors[continent]  , image} // Add color here
+    ];
+  });
 
   const toggleContinentVisibility = (continent: keyof ContinentVisibility) => {
     setVisibleContinents((prev: any) => ({
@@ -203,50 +141,143 @@ const Read_excel = () => {
     setIsAnimating(!isAnimating);
   };
 
-  const datasets = [
-    {
-      label: null,
-      data: sortedPopulations,
-      backgroundColor: sortedCountries.map(
-        (country) => continentColors[continentMap[country]]
-      ),
-      borderColor: sortedCountries.map((country) =>
-        continentColors[continentMap[country]]?.replace(/0.6/, "1")
-      ),
-      borderWidth: 1,
-    },
-  ];
-
-  const data: any = {
-    labels: sortedCountries,
-    datasets: datasets,
+  const getSubtitle = () => {
+    if (!excelData) return "";
+    return `<span style="font-size: 80px">${selectedYear}</span>
+        <br>
+        <span style="font-size: 22px">
+            Total: <b>${totalPopulation.toLocaleString(undefined, {
+              maximumFractionDigits: 0,
+            })}</b>
+        </span>`;
   };
 
-  const options: any = {
-    indexAxis: "y",
-    responsive: true,
-    scales: {
-      x: {
-        position: "top",
-        grid: {
-          drawOnChartArea: false,
-        },
-      },
-      x2: {
-        position: "bottom",
-        type: "category",
-        labels: selectedYears,
-        grid: {
-          drawOnChartArea: false,
-        },
-      },
-    },
-    plugins: {
-      legend: {
-        display: false,
-      },
-    },
+  const animatePopulationData = (startData: number[], endData: number[]) => {
+    const steps = 100; 
+    const increment = endData.map((end, index) => (end - startData[index]) / steps);
+    let currentStep = 0;
+  
+    const animate = () => {
+      // Animate the data until the current step exceeds the number of steps
+      if (currentStep < steps) {
+        const updatedData = startData.map((start, index) => Math.round(start + increment[index] * currentStep));
+        setPopulationData(updatedData);
+        currentStep++;
+        requestAnimationFrame(animate);
+      } else {
+        // When finished, set the population data to endData
+        setPopulationData(endData);
+      }
+    };
+  
+    animate(); 
   };
+
+  
+  const chartOptions = {
+    chart: {
+      animation: true,
+      height: '40%', 
+    },
+    subtitle: {
+      useHTML: true,
+      text: getSubtitle(),
+      floating: true,
+      align: "right",
+      verticalAlign: "bottom",
+      y: 0,
+      x: -100,
+    },
+    legend: {
+      enabled: false,
+    },
+    xAxis: {
+      type: "category",
+    },
+    yAxis: {
+      opposite: true,
+      tickPixelInterval: 150,
+      title: {
+        text: null,
+      },
+    },
+    plotOptions: {
+      series: {
+        animation: {
+          duration: 500
+        },
+        groupPadding: 0,
+        pointPadding: 0.1,
+        borderWidth: 0,
+        colorByPoint: true,
+        dataSorting: {
+          enabled: true,
+          matchByName: true,
+        },
+        type: "bar",
+        dataLabels: {
+          enabled: true,
+        },
+      },
+    },
+    series: [
+      {
+        type: "bar",
+        name: selectedYear.toString(),
+        data: outputArray.map(([country, population, color]) => ({
+          name: country,
+          y: population,
+          color: color.color, // Apply the continent color here
+          image: color.image // Include the image here
+        })),
+        dataLabels: {
+          enabled: true,
+          useHTML: true,
+          formatter: function () {
+            return `
+              <div style="display: flex; align-items: center; ">
+                <img src="${this.point.image}" alt="${this.point.name}" style="width: 20px; height: 20px; margin-right: 5px;" />
+                ${this.y.toLocaleString()}
+              </div>
+            `;
+          },
+        },
+        
+      },
+    ],
+    responsive: {
+      rules: [{
+          condition: {
+              maxWidth: 550
+          },
+          chartOptions: {
+              xAxis: {
+                  visible: false
+              },
+              subtitle: {
+                  x: 0
+              },
+              plotOptions: {
+                  series: {
+                      dataLabels: [{
+                          enabled: true,
+                          y: 8
+                      }, {
+                          enabled: true,
+                          format: '{point.name}',
+                          y: -8,
+                          style: {
+                              fontWeight: 'normal',
+                              opacity: 0.7
+                          }
+                      }]
+                  }
+              }
+          }
+      }]
+  }
+  };
+  
 
   return (
     <div
@@ -257,32 +288,36 @@ const Read_excel = () => {
         boxSizing: "border-box",
       }}
     >
-       <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            fontSize: "1.5rem",
-            fontWeight: "bold",
-          }}
-        >
-          Population growth per country 1950 to 2021
-        </div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          fontSize: "2rem",
+          fontWeight: "bold",
+          marginBottom: "20px",
+        }}
+      >
+        Population growth per country 1950 to 2021
+      </div>
 
-
-
-     
-
-        <div style={{  display: "flex",     alignItems: "center", gap : 600}}>
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          flexWrap: "wrap",
+          gap: "20px",
+        }}
+      >
         <button
           style={{
-            padding: 10,
-            fontSize: 16,
+            padding: "10px",
+            fontSize: "16px",
             color: "white",
-            backgroundColor: "rgba(0,0,0,0.6)", // ใช้ backgroundColor แทน background-color
+            backgroundColor: "rgba(0,0,0,0.6)",
             border: "none",
-            borderRadius: "100%",
+            borderRadius: "50%",
             cursor: "pointer",
-            marginLeft: 100,
             display: "flex",
             justifyContent: "center",
             alignItems: "center",
@@ -295,90 +330,54 @@ const Read_excel = () => {
             <FaPlay style={{ color: "#fff" }} />
           )}
         </button>
-       
-  
-      <div
-        style={{
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-          flexWrap: "wrap",
-        }}
-      >
-        <span style={{ fontWeight: "bold" }}> Region :</span>
-        {Object.keys(continentColors).map((continent: any) => (
-          <div
-            style={{ display: "flex", alignItems: "center" }}
-            key={continent}
-          >
-            <button
-              onClick={() => toggleContinentVisibility(continent)}
-              style={{
-                backgroundColor: visibleContinents[continent]
-                  ? continentColors[continent]
-                  : "#ccc",
-                color: "white",
-                border: "none",
-                padding: "8px",
-                margin: "10px 5px",
-                // borderRadius: "3px",
-                cursor: "pointer",
-                fontSize: "14px",
-              }}
-            ></button>
-            <span>{continent}</span>
-          </div>
-        ))}
-      </div>
-      </div>
-      {/* Chart container */}
-      <div style={{ position: "relative", width: "100%", height: "100%" }}>
-        <Bar
-          data={data}
-          options={options}
-          width={window.innerWidth}
-          height={window.innerHeight - 150}
-        />
 
         <div
           style={{
-            position: "absolute",
-            bottom: "100px",
-            right: "10px",
             display: "flex",
-            flexDirection: "column",
-            alignItems: "flex-end",
+            justifyContent: "center",
+            alignItems: "center",
+            flexWrap: "wrap",
           }}
         >
-          {/* Selected year */}
-          <p
-            style={{
-              fontSize: "5vw", // Default for desktop
-              color: "rgba(0, 0, 0, 0.6)",
-              fontWeight: "bold",
-              margin: 0,
-            }}
-          >
-            {selectedYear}
-          </p>
-
-          {/* Total population */}
-          <p
-            style={{
-              fontSize: "3vw", // Default for desktop
-              color: "rgba(0, 0, 0, 0.6)",
-              margin: 0,
-            }}
-          >
-            Total:{" "}
-            {totalPopulation.toLocaleString(undefined, {
-              maximumFractionDigits: 0,
-            })}
-          </p>
+          <span style={{ fontWeight: "bold" }}>Region:</span>
+          {Object.keys(continentColors).map((continent: any) => (
+            <div
+              style={{ display: "flex", alignItems: "center" }}
+              key={continent}
+            >
+              <button
+                onClick={() => toggleContinentVisibility(continent)}
+                style={{
+                  backgroundColor: visibleContinents[continent]
+                    ? continentColors[continent]
+                    : "#ccc",
+                  color: "white",
+                  border: "none",
+                  padding: "8px",
+                  margin: "10px 5px",
+                  cursor: "pointer",
+                  fontSize: "14px",
+                }}
+              ></button>
+              <span>{continent}</span>
+            </div>
+          ))}
         </div>
+      </div>
+
+      <div style={{ position: "relative", width: "100%", height: "100%" }}>
+        <HighchartsReact
+          highcharts={Highcharts}
+          options={chartOptions}
+          ref={chartRef}
+        
+        />
+
       </div>
     </div>
   );
 };
 
 export default Read_excel;
+
+
